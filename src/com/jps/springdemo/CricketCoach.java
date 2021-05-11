c5c4f582f9b7453325d26277f0e38f405ddc3818
Anvesh-Chinta <pratanvesh@gmail.com>
2021-05-10 19:58:28 -0500

    Refactored code:Inversion of Control with XML Configuration Activity#1

---
+++ src/com/jps/springdemo/CricketCoach.java
@@ -0,0 +1,12 @@
+package com.jps.springdemo;
+
+public class CricketCoach implements Coach {
+
+	@Override
+	public String getDailyWorkout() {
+		
+		return "Three rounds followed by net practice!";
+		
+	}
+
+}
