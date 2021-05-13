8c077a73da86575d5f919d8a6f3c6964b5e8146c
Anvesh-Chinta <pratanvesh@gmail.com>
2021-05-13 00:23:31 -0500

    Dependency Injection Pt2.:     Configure the dependency injection on spring config file(applicationContext.xml) & implement in SetterDemoApp similar to SpringCoreApp- setter method

--- src/com/jps/springdemo/CricketCoach.java
+++ src/com/jps/springdemo/CricketCoach.java
@@ -5,6 +5,8 @@ import com.jps.springdemo.service.FortuneService;
 public class CricketCoach implements Coach {
 
 	private FortuneService fortuneService;
+	private String emailAddress;
+	private String team;
 	
 	//create a no-arg constructor
 	public CricketCoach() {
@@ -13,10 +15,29 @@ public class CricketCoach implements Coach {
 	
 	//our setter method
 	public void setFortuneService(FortuneService fortuneService) {
-		System.out.println("CricketCoach: inside setter method");
+		System.out.println("CricketCoach: inside setter method - setFortuneService");
 		this.fortuneService = fortuneService;
 	}
 
+	//We generally need setters but we are using getters to print the value here...
+
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+
+	public void setEmailAddress(String emailAddress) {
+		System.out.println("CricketCoach: inside setter method - setEmailAddress");
+		this.emailAddress = emailAddress;
+	}
+
+	public String getTeam() {
+		return team;
+	}
+
+	public void setTeam(String team) {
+		System.out.println("CricketCoach: inside setter method - setTeam");
+		this.team = team;
+	}
 
 	@Override
 	public String getDailyWorkout() {
