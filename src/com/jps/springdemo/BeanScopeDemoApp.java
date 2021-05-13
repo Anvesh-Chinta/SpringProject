71aa63868e82bc994d93e89bd53aec9a7e7427c4
Anvesh-Chinta <pratanvesh@gmail.com>
2021-05-13 12:56:00 -0500

    Bean Scope - Singleton: Create config file and demo app(main)

---
+++ src/com/jps/springdemo/BeanScopeDemoApp.java
@@ -0,0 +1,18 @@
+package com.jps.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class BeanScopeDemoApp {
+
+	public static void main(String[] args) {
+		
+		//load the spring config file
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("singleton-beanScope-applicationContext.xml");
+		
+		//retrieve bean from spring container
+		Coach theCoach = context.getBean("myCoach", Coach.class);
+		
+		Coach alphaCoach = context.getBean("myCoach", Coach.class);
+	}
+
+}
