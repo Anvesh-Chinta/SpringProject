e369e29e8ba5893e9f09731f79d0ea234b57bdee
Anvesh-Chinta <pratanvesh@gmail.com>
2021-05-13 00:56:41 -0500

    Injecting Values from a Properties file:     Reference values from a property file :: Problem#2

--- src/com/jps/springdemo/service/DailyFortuneService.java
+++ src/com/jps/springdemo/service/DailyFortuneService.java
@@ -1,11 +1,24 @@
 package com.jps.springdemo.service;
 
+import java.util.Random;
+
 public class DailyFortuneService implements FortuneService {
 
 	@Override
 	public String getFortune() {
-		// TODO Auto-generated method stub
-		return "Your team will win todays match!";
+		
+		//define three fortunes in an array
+		String[] dailyFortunes =  {"Your team will win todays match!", 
+				"Odds are in your favour", 
+				"You are no less than others!"
+				};
+		
+		//random fortune from array
+		Random random = new Random();
+		
+		int index = random.nextInt(dailyFortunes.length);
+		String theFortune = dailyFortunes[index];
+		return theFortune;
 	}
 
 }
