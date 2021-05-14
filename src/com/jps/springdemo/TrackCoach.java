37a94203c6c9ff0cd7aebf134124e5752728e42a
Anvesh-Chinta <pratanvesh@gmail.com>
2021-05-14 00:17:44 -0500

    Bean LifeCycle: Define your methods for init and destroy

--- src/com/jps/springdemo/TrackCoach.java
+++ src/com/jps/springdemo/TrackCoach.java
@@ -25,5 +25,19 @@ public class TrackCoach implements Coach {
 	public String getDailyFortune() {
 		return "Just Do It: " + fortuneService.getFortune();
 	}
+	
+	//add an init method
+	public void startupStuff() {
+	
+		System.out.println("TrackCoach: inside method startupStuff");
+	
+	}
+	
+	//add a destroy method
+	public void cleanupStuff() {
+		
+		System.out.println("TrackCoach: inside method cleanupStuff");
+		
+	}
 
 }
