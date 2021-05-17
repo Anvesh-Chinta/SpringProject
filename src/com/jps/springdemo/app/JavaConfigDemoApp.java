17b4d7a557e656640cfaddd60fb37ea1fc401107
Anvesh-Chinta <pratanvesh@gmail.com>
2021-05-17 14:19:53 -0500

    Spring Configuration with Java Code: Read Spring Java configuration class

---
+++ src/com/jps/springdemo/app/JavaConfigDemoApp.java
@@ -0,0 +1,14 @@
+package com.jps.springdemo.app;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class JavaConfigDemoApp {
+
+	public static void main(String[] args) {
+
+		// read spring config file
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SportConfig.class);
+
+	}
+
+}
