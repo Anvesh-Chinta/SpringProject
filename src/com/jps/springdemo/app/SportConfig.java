888c8c145c33a10eb78075804bd2b4e76c4b8798
Anvesh-Chinta <pratanvesh@gmail.com>
2021-05-17 14:17:51 -0500

    Spring Configuration with Java Code: Add component scanning support: @ComponentScan (optional)

--- src/com/jps/springdemo/app/SportConfig.java
+++ src/com/jps/springdemo/app/SportConfig.java
@@ -1,8 +1,10 @@
 package com.jps.springdemo.app;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ComponentScan("com.jps.springdemo")
 public class SportConfig {
 
 }
