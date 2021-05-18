b853487abbe3b65d9fa026fad73f908cf304b26f
Anvesh-Chinta <pratanvesh@gmail.com>
2021-05-17 19:57:20 -0500

    Add Logging Messages in Spring 5.1 - All Java Config Version - All Java Config (no xml) - 0. Create a logging properties file 1. Create a logger class

--- src/com/jps/springdemo/config/MyLoggerConfig.java
+++ src/com/jps/springdemo/config/MyLoggerConfig.java
@@ -8,33 +8,11 @@ import java.util.logging.Logger;
 
 import java.util.logging.SimpleFormatter;
 
-import javax.annotation.PostConstruct;
-
-import org.springframework.beans.factory.annotation.Value;
-
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-
-import org.springframework.context.annotation.Configuration;
-
-import org.springframework.context.annotation.PropertySource;
-
-@Configuration
-
-@PropertySource("classpath:mylogger.properties")
 
 public class MyLoggerConfig {
 
-	@Value("${root.logger.level}")
-
-	private String rootLoggerLevel;
-
-	@Value("${printed.logger.level}")
-
-	private String printedLoggerLevel;
-
-	@PostConstruct
-
-	public void initLogger() {
+	public MyLoggerConfig(String rootLoggerLevel, String printedLoggerLevel) {
 
 // parse levels 
 
