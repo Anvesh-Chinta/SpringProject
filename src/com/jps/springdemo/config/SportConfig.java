88ec95a2e448e5edfe7ed4921515e2910864bda3
Anvesh-Chinta <pratanvesh@gmail.com>
2021-05-19 15:07:08 -0500

    Inject Bean dependencies

--- src/com/jps/springdemo/config/SportConfig.java
+++ src/com/jps/springdemo/config/SportConfig.java
@@ -5,13 +5,23 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 import com.jps.springdemo.app.Coach;
+import com.jps.springdemo.app.FortuneService;
+import com.jps.springdemo.app.SadFortuneService;
 import com.jps.springdemo.app.SwimCoach;
 
 @Configuration
 @ComponentScan("com.jps.springdemo")
 public class SportConfig {
+
+	//define bean  for our sad fortune service
+	@Bean
+	public FortuneService sadFortuneService() {
+		
+		return new SadFortuneService();
+		
+	}
 	
-	//define bean for our swim coach 
+	//define bean for our swim coach and inject dependency
 	@Bean
 	public Coach swimCoach() {
 		
