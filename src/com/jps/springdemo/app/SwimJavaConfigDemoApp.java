dff789ab00bfba329355a8c8c8cfb1b443f7fe22
Anvesh-Chinta <pratanvesh@gmail.com>
2021-05-19 15:10:34 -0500

    Retrieve bean from Spring container

--- src/com/jps/springdemo/app/SwimJavaConfigDemoApp.java
+++ src/com/jps/springdemo/app/SwimJavaConfigDemoApp.java
@@ -9,7 +9,18 @@ public class SwimJavaConfigDemoApp {
 		// read spring config file
 		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SportConfig.class);
 
-		
+		// get bean from Spring container - default bean id
+		Coach theCoach = context.getBean("tennisCoach", Coach.class);
+
+		// call methods on the bean
+		System.out.println(theCoach.getDailyWorkout());
+
+		// call the method to get the daily fortune
+		System.out.println(theCoach.getDailyFortune());
+
+		// close the context
+		context.close();
+
 	}
 
 }
