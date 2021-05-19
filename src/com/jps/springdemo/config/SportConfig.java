5c298afdd819d1dd1cc609cc9a8fa89adb5a76bb
Anvesh-Chinta <pratanvesh@gmail.com>
2021-05-19 15:05:22 -0500

    Define method to expose bean

--- src/com/jps/springdemo/config/SportConfig.java
+++ src/com/jps/springdemo/config/SportConfig.java
@@ -1,6 +1,22 @@
 package com.jps.springdemo.config;
 
-public class SportConfig {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
+import com.jps.springdemo.app.Coach;
+import com.jps.springdemo.app.SwimCoach;
+
+@Configuration
+@ComponentScan("com.jps.springdemo")
+public class SportConfig {
+	
+	//define bean for our swim coach 
+	@Bean
+	public Coach swimCoach() {
+		
+		return new SwimCoach(null);
+		
+	}
 	
 }
