45b5d3be33118de44bae07ff8b547e7a812d04d9
Anvesh-Chinta <pratanvesh@gmail.com>
2021-05-20 00:00:00 -0500

    PA#7: Created new Fortune service implementation(return a single hard-coded fortuen))

---
+++ src/com/jps/springdemo/app/SeriousFortuneService.java
@@ -0,0 +1,12 @@
+package com.jps.springdemo.app;
+
+public class SeriousFortuneService implements FortuneService {
+
+	@Override
+	public String getFortune() {
+		
+		return "Bulls Eye!";
+	
+	}
+
+}
