b701540f7f55e1fa13243cb2e9966d667036cbe8
Anvesh-Chinta <pratanvesh@gmail.com>
2021-05-19 23:55:03 -0500

    PA#7: Created new Coach Implementation

--- src/com/jps/springdemo/app/ArcheryCoach.java
+++ src/com/jps/springdemo/app/ArcheryCoach.java
@@ -1,8 +1,11 @@
 package com.jps.springdemo.app;
 
+import org.springframework.stereotype.Component;
 
+@Component
 public class ArcheryCoach implements Coach {
 
+	
 	@Override
 	public String getDailyWorkout() {
 		
