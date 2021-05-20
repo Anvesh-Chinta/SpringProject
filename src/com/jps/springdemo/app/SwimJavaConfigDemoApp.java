ac978337132906f17950fe757626e26e81791868
Anvesh-Chinta <pratanvesh@gmail.com>
2021-05-19 23:42:23 -0500

    Injecting Values from Property File: test app

--- src/com/jps/springdemo/app/SwimJavaConfigDemoApp.java
+++ src/com/jps/springdemo/app/SwimJavaConfigDemoApp.java
@@ -10,13 +10,18 @@ public class SwimJavaConfigDemoApp {
 		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SportConfig.class);
 
 		// get bean from Spring container - default bean id
-		Coach theCoach = context.getBean("swimCoach", Coach.class);
+		SwimCoach theCoach = context.getBean("swimCoach", SwimCoach.class);
 
 		// call methods on the bean
 		System.out.println(theCoach.getDailyWorkout());
 
 		// call the method to get the daily fortune
 		System.out.println(theCoach.getDailyFortune());
+		
+		//call our new swim coach methods... which have properties value injected
+		System.out.println("\nemail: " + theCoach.getEmail());
+		
+		System.out.println("\nteam: " + theCoach.getTeam());
 
 		// close the context
 		context.close();
